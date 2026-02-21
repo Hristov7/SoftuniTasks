2d34b8b69cdd6f22637ce0861d1059511a5f13d6
Hristov7 <danielantonov07@gmail.com>
2026-02-21 15:39:41 +0200

    V-Logger

---
+++ Rankings/Program.cs
@@ -0,0 +1,114 @@
+﻿namespace Rankings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> map = new();
+            string input;
+            while ((input = Console.ReadLine()) != "end of contests")
+            {
+                string[] parts = input.Split(':', StringSplitOptions.RemoveEmptyEntries);
+                if (!map.ContainsKey(parts[0]))
+                {
+                    map[parts[0]] = parts[1];
+                }
+            }
+
+            Dictionary<string, Dictionary<string,int>> users = new();
+            while ((input = Console.ReadLine()) != "end of submissions")
+            {
+                string[] data = input.Split("=>", StringSplitOptions.RemoveEmptyEntries);
+                if (!users.ContainsKey(data[2]))
+                {
+                    if (map.ContainsKey(data[0]))
+                    {
+                        string password = map[data[0]];
+                        if (password == data[1])
+                        {
+                            users[data[2]] = new()
+                            {
+                                { data[0], int.Parse(data[3]) }
+                            };
+                        }
+                    }
+                }
+                else
+                {
+                    Dictionary<string, int> pairs = users[data[2]];
+                    if (pairs.ContainsKey(data[0]))
+                    {
+                        if (pairs[data[0]] < int.Parse(data[3]))
+                        {
+                            pairs[data[0]] = int.Parse(data[3]);
+                        }
+                    }
+                    else
+                    {
+                        users[data[2]].Add(data[0], int.Parse(data[3]));
+                    }
+                }
+            }
+
+            Dictionary<string, int> personWithMostPoints = new();
+            foreach (KeyValuePair<string, Dictionary<string, int>> user in users)
+            {
+                int total = 0;
+                foreach (KeyValuePair<string, int> pair in user.Value)
+                {
+                    total += pair.Value;
+                }
+
+                if (!personWithMostPoints.ContainsKey(user.Key))
+                {
+                    personWithMostPoints[user.Key] = total;
+                }
+            }
+            int maxPoints = personWithMostPoints.Max(x => x.Value);
+            string userWithMaxPoints = personWithMostPoints.FirstOrDefault(x => x.Value == maxPoints).Key;
+            Console.WriteLine($"Best candidate is {userWithMaxPoints} with total {maxPoints} points.");
+            Console.WriteLine("Ranking:");
+            foreach (KeyValuePair<string, Dictionary<string, int>> user in users.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{user.Key}");
+                foreach (KeyValuePair<string, int> pair in user.Value.OrderByDescending(x=>x.Value))
+                {
+                    Console.WriteLine($"# {pair.Key} -> {pair.Value}");
+                }
+            }
+        }
+    }
+}
+/*
+Part One Interview:success
+Js Fundamentals:JSFundPass
+C# Fundamentals:fundPass
+Algorithms:fun
+end of contests
+C# Fundamentals=>fundPass=>Tanya=>350
+Algorithms=>fun=>Tanya=>380
+Part One Interview=>success=>Nikola=>120
+Java Basics Exam=>JSFundPass=>Parker=>400
+Part One Interview=>success=>Tanya=>220
+OOP Advanced=>password123=>BaiIvan=>231
+C# Fundamentals=>fundPass=>Tanya=>250
+C# Fundamentals=>fundPass=>Nikola=>200
+Js Fundamentals=>JSFundPass=>Tanya=>400
+end of submissions
+
+Java Advanced:funpass
+Part Two Interview:success
+Math Concept:asdasd
+Java Web Basics:forrF
+end of contests
+Math Concept=>ispass=>Monika=>290
+Java Advanced=>funpass=>Simon=>400
+Part Two Interview=>success=>Drago=>120
+Java Advanced=>funpass=>Petyr=>90
+Java Web Basics=>forrF=>Simon=>280
+Part Two Interview=>success=>Petyr=>0
+Math Concept=>asdasd=>Drago=>250
+Part Two Interview=>success=>Simon=>200
+end of submissions
+
+ */
---
+++ Vlogger/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace Vlogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, (HashSet<string> followers, HashSet<string> following)> vloggers = new();
+
+            string input;
+            while ((input = Console.ReadLine()) != "Statistics")
+            {
+                if (input.EndsWith("joined The V-Logger"))
+                {
+                    string name = input.Split(" ")[0];
+                    if (!vloggers.ContainsKey(name))
+                    {
+                        vloggers[name] = (new HashSet<string>(), new HashSet<string>());
+                    }
+                }
+                else if (input.Contains(" followed "))
+                {
+                    string[] parts = input.Split(" followed ");
+                    string follower = parts[0];
+                    string followed = parts[1];
+
+                    if (!vloggers.ContainsKey(follower) || !vloggers.ContainsKey(followed))
+                        continue;
+                    if (follower == followed)
+                        continue;
+                    if (vloggers[followed].followers.Contains(follower))
+                        continue;
+
+                    vloggers[followed].followers.Add(follower);
+                    vloggers[follower].following.Add(followed);
+                }
+            }
+
+            Console.WriteLine($"The V-Logger has a total of {vloggers.Count} vloggers in its logs.");
+
+            var ordered = vloggers
+                .OrderByDescending(v => v.Value.followers.Count)
+                .ThenBy(v => v.Value.following.Count)
+                .ToList();
+
+            for (int i = 0; i < ordered.Count; i++)
+            {
+                var vlogger = ordered[i];
+                string name = vlogger.Key;
+                int followersCount = vlogger.Value.followers.Count;
+                int followingCount = vlogger.Value.following.Count;
+
+                Console.WriteLine($"{i + 1}. {name} : {followersCount} followers, {followingCount} following");
+
+                if (i == 0)
+                {
+                    foreach (string follower in vlogger.Value.followers.OrderBy(f => f))
+                    {
+                        Console.WriteLine($"* {follower}");
+                    }
+                }
+            }
+        }
+    }
+}
+/*
+EmilConrad joined The V-Logger
+VenomTheDoctor joined The V-Logger
+Saffrona joined The V-Logger
+Saffrona followed EmilConrad
+Saffrona followed VenomTheDoctor
+EmilConrad followed VenomTheDoctor
+VenomTheDoctor followed VenomTheDoctor
+Saffrona followed EmilConrad
+Statistics
+
+JennaMarbles joined The V-Logger
+JennaMarbles followed Zoella
+AmazingPhil joined The V-Logger
+JennaMarbles followed AmazingPhil
+Zoella joined The V-Logger
+JennaMarbles followed Zoella
+Zoella followed AmazingPhil
+Christy followed Zoella
+Zoella followed Christy
+JacksGap joined The V-Logger
+JacksGap followed JennaMarbles
+PewDiePie joined The V-Logger
+Zoella joined The V-Logger
+Statistics
+ */
